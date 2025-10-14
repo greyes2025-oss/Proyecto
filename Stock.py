f63cbe94327090c5fc03b685d3afe2c7bfa5403f
cparra2007 <parracristopher04@gmail.com>
2025-10-14 18:51:38 -0300

    Agrego  cambios a stock y el .gitignore

--- Stock.py
+++ Stock.py
@@ -5,33 +5,25 @@ from typing import List, Dict
 class Stock:
     def __init__(self):
         """
-        constructor de la clase stock.
-        Se utiliza un diccionario para un acceso y modificación de datos optimiizada
-        La clave es el nombre del ingrediente  y el valor es el objeto ingrediente
+        Constructor de la clase Stock
         """
         self.ingredientes: Dict[str, Ingrediente] = {}
 
     def agregar(self, nombre: str, unidad: str, cantidad: float):
         """
-        agrega un nuevo ingrediente al stock o actualiza la cantidad si ya existe
-        
+        Agrega un nuevo ingrediente al stock o actualiza la cantidad si ya existe
         """
-        # Convertimos el nombre a minusculas
-        nombre = nombre.lower()
-        
-        # Si el ingrediente ya existe se suma la cantidad
+    
         if nombre in self.ingredientes:
             self.ingredientes[nombre].cantidad += cantidad
-        # Si es nuevo crea una instancia de ingrediente y la añadimos al diccionario
         else:
             self.ingredientes[nombre] = Ingrediente(nombre=nombre, unidad=unidad, cantidad=cantidad)
 
     def eliminar(self, nombre_ingrediente: str) -> bool:
         """
-        elimina un ingrediente del stock por su nombre
-        Devuelve true si se elimino false si no se encontro
+        Elimina un ingrediente del stock por su nombre
         """
-        nombre_ingrediente = nombre_ingrediente.lower()
+
         if nombre_ingrediente in self.ingredientes:
             del self.ingredientes[nombre_ingrediente]
             return True
@@ -39,11 +31,9 @@ class Stock:
 
     def descontar(self, nombre_ingrediente: str, cantidad_a_descontar: float) -> bool:
         """
-        reduce la cantidad de un ingrediente especifico .
-        devuelve true si se desconto false si no hay suficiente stock
+        Reduce la cantidad de un ingrediente especifico
         """
-        nombre_ingrediente = nombre_ingrediente.lower()
-        # Comprueba si el ingrediente existe y si hay suficiente cantidad
+        
         if nombre_ingrediente in self.ingredientes and self.ingredientes[nombre_ingrediente].cantidad >= cantidad_a_descontar:
             self.ingredientes[nombre_ingrediente].cantidad -= cantidad_a_descontar
             return True
